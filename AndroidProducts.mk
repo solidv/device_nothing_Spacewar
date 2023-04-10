c7a33fba63388df19592d717deeed15c0008bac6
solidv <pedroclaro2@gmail.com>
2023-04-10 20:36:39 +0100

    Spacewar: Pixel Experience bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/pixys_Spacewar.mk
+    $(LOCAL_DIR)/aosp_Spacewar.mk
 
 COMMON_LUNCH_CHOICES := \
-    pixys_Spacewar-eng \
-    pixys_Spacewar-userdebug \
-    pixys_Spacewar-user
+    aosp_Spacewar-eng \
+    aosp_Spacewar-userdebug \
+    aosp_Spacewar-user
